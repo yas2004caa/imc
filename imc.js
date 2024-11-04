75d97b5d2e2908cc18b97c167eaf84b5613128e8
yas2004caa <yas2004capis@gmail.com>
2024-11-04 11:45:04 -0300

    Update imc.js

--- imc.js
+++ imc.js
@@ -13,12 +13,12 @@ function imc(){
         calcularIMC(){
             if (this.altura > 3) this.altura=this.altura/100
             const imc = this.peso/this.altura**2;
-            if (imc < 18.5 ) this.classificacao = "obesidade morbida";
+            if (imc < 18.5 ) this.classificacao = "magreza";
             if (imc >= 18.5 &&  imc < 25 ) this.classificacao = "saudavel"
             if (imc >= 25 &&  imc < 30 ) this.classificacao = "sobrepeso"
             if (imc >= 30 &&  imc < 35 ) this.classificacao = "obesidade"
             if (imc >= 35 &&  imc < 40 ) this.classificacao = "obesidade Severa"
-            if (imc >= 40 ) this.classificacao = "magreza"
+            if (imc >= 40 ) this.classificacao = "obesidade morbida"
             return imc.toFixed(2);
         
             
